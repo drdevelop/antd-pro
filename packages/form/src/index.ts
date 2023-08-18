5717f5281e5cf06f9c93db51e2f9bbe9f851153f
drdevelop <cxd2534118085@163.com>
2023-08-19 01:00:01 +0800

    fix: export 'RefCurrent' (reexported as 'RefCurrent') was not found

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -1,3 +1,7 @@
-export { default, RefCurrent } from './core';
+import { RefCurrent as SchemaFormRefCurrent } from './core/Transformer/SchemaForm';
+
+export type RefCurrent = SchemaFormRefCurrent;
+
+export { default } from './core';
 
 export { registerComponent, registerComponents } from './components';
