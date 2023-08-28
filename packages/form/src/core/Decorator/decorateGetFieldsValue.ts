f8ef5cabf9b9ff57a51edd8e65cfb125974effa5
drdevelop <cxd2534118085@163.com>
2023-08-28 21:37:14 +0800

    fix: getFieldsValue passing parameters not working

--- packages/form/src/core/Decorator/decorateGetFieldsValue.ts
+++ packages/form/src/core/Decorator/decorateGetFieldsValue.ts
@@ -1,11 +1,12 @@
 import { FormInstance } from 'antd';
 import { fissionValue } from '../Transformer/valueAtomize';
 import { Schema } from '../types';
+import { NamePath } from 'antd/es/form/interface';
 
 export default (shadowForm: FormInstance, instance: FormInstance, schema: Schema) => {
   const oldGetFieldsValue = instance.getFieldsValue;
-  shadowForm.getFieldsValue = () => {
-    const values = oldGetFieldsValue();
+  shadowForm.getFieldsValue = (...args) => {
+    const values = oldGetFieldsValue(...(args as [true | NamePath[], (meta: any) => boolean]));
     return fissionValue(schema, values);
   };
 };
