86e112732bd10aca21f54629c7253d684551314d
drdevelop <cxd2534118085@163.com>
2023-10-16 20:35:28 +0800

    fix: [Type]Schema type did not export from entry

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -2,6 +2,7 @@ import { Props, RefCurrent as SchemaFormRefCurrent } from './core/Transformer/Sc
 
 export type RefCurrent = SchemaFormRefCurrent;
 export type SchemaFormProps<T = ''> = Props<T>;
+export type Schema<T = ''> = Props<T>['schema'];
 
 export { default } from './core';
 
